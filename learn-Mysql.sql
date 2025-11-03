90d16ae26ca0eb2626fe6518331fabcde9cdfd20
Gowthampoli <poli.gowthamkumarreddy@gmail.com>
2025-11-03 12:08:52 +0530

    Full Stack

---
+++ learn-Mysql.sql
@@ -0,0 +1,18 @@
+mysql -uroot -proot
+system cls;
+show databases;
+
+CREATE DATABASE data;
+USE data;
+
+SHOW TABLES;
+
+CREATE TABLES employees (
+    eid int,
+    ename varchar(32),
+    esal int
+);
+
+DESCRIBE employees; /* OR */ DESC employees;
+
+INSERT INTO employees VALUES (101, 'Rahul', 45000.45), (102, 'Sonia', 55000.55), (103, 'Priya', 65000.65);
