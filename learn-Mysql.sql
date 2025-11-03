fc3221a17c2a1e8446afbd4adb337743814a2626
Gowthampoli <poli.gowthamkumarreddy@gmail.com>
2025-11-03 12:14:29 +0530

    added a line by gowtham

--- learn-Mysql.sql
+++ learn-Mysql.sql
@@ -18,3 +18,5 @@ DESCRIBE employees; /* OR */ DESC employees;
 INSERT INTO employees VALUES (101, 'Rahul', 45000.45), (102, 'Sonia', 55000.55), (103, 'Priya', 65000.65);
 
 SELECT * FROM employees;
+
+UPDATE employees SET esal = 70000.00 WHERE eid = 102;
