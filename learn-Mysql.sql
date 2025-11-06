46c89ec5800c096621aa948ad4bbc1d3583c86ca
Gowthampoli <poli.gowthamkumarreddy@gmail.com>
2025-11-06 11:55:54 +0530

    constrints

--- learn-Mysql.sql
+++ learn-Mysql.sql
@@ -71,3 +71,27 @@ INSERT INTO employees (eid,ename,age,location) VALUES(101,'Rahul',48,'mumbai'),(
 CREATE TABLE employees (eid int UNIQUE KEY , ename varchar(32) NOT NULL, esal float(10,2) CHECK (esal>30000), age int CHECK (age>18), location varchar(32) DEFAULT 'Bangalore');
 INSERT INTO employees (eid,ename,esal,age,location) VALUES(101,'Rahul',5000.45,48,'mumbai'),(102,'Soniya',55000.55,58,'patna'),(103,'Priya',25000.35,40,'mumbai');
 
+===================================
+class 4
+
+CREATE TABLE orders(order_id int UNIQUE KEY,detailes varchar(32) NOT NULL, Amount float(10,2) CHECK(Amount>500),Status varchar(32) DEFAULT "Open", PRIMARY KEY(order_id));
+INSERT INTO orders VALUES(201,'order1',1500.50,'Open'),(202,'order2',2500.75,'Closed'),(203,'order3',3500.25);
+INSERT INTO orders (order_id,detailes,Amount) VALUES(204,'order4',4500.55),(205,'order5',500.65);
+
+ALTER TABLE orders ADD PRIMARY KEY(order_id);
+INSERT INTO orders (detailes,Amount) VALUES('order6',6500.85);
+DELETE FROM orders WHERE order_id=204;
+DELETE FROM orders WHERE order_id=205;
+INSERT INTO orders (order_id,detailes,Amount) VALUES(204,'order4',6500.85);
+
+
+CREATE DATABASE server;
+CREATE TABLE customer (customer_id int, Name varchar(32) NOT NULL, Gender varchar(10) NOT NULL, location varchar(32) DEFAULT "Bangalore",PRIMARY KEY(customer_id));
+INSERT INTO customer VALUES (101,'Rahul','Male','Delhi'),(102,'Soniya','Female','New Delhi'),(103,'Priya','Female','Mumbai'),(104,'Modi','Male','Gujarat');
+
+CREATE TABLE orders (order_id int PRIMARY KEY,detailes varchar(32) NOT NULL, Amount float NOT NULL, customer_id int, Status varchar(32) DEFAULT "open",FOREIGN KEY(customer_id) REFERENCES customer(customer_id));
+INSERT INTO orders VALUES(1001,'MP1',350,101,'closed'),(1002,'Lenovo Mouse',450,102,'closed');
+INSERT INTO orders (order_id,detailes,Amount,customer_id) VALUES (1003,'Lenovo Laptop',65000,102),(1004,'MP2',1050,101),(1005,'Mobile',22000,104),(1006,'MP1',350,104),(1007,'Iphone 17 pro',120000,104);
+UPDATE orders SET Status='inprogress' WHERE ORDER_ID=1003;
+UPDATE orders SET Status='inprogress' WHERE ORDER_ID=1005;
+UPDATE orders SET Status='inprogress' WHERE ORDER_ID=1007;
